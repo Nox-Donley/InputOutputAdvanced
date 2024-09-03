fe0bdfdd71854744cd71bc88030f033488c53a10
Nox-Donley <145069348+Nox-Donley@users.noreply.github.com>
2024-09-03 15:22:31 -0600

    Create inputoutput.py

---
+++ inputoutput.py
@@ -0,0 +1,12 @@
+for i in range(1, 18):
+    print(5*i)
+
+j=5
+while j != 90:
+    print(j)
+    j= j+5
+   
+mblurgle = input("forgle")
+while mblurgle != "boop":
+    mblurgle = input("forgle")
+    
